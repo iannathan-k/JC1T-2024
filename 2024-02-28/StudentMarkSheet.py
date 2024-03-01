42e3080d0e185d22bc3cf985e397133dd20ba379
iannathan-k <ianmtv@gmail.com>
2024-03-01 09:15:31 +0700

    01-03-24

--- 2024-02-28/StudentMarkSheet.py
+++ 2024-02-28/StudentMarkSheet.py
@@ -3,8 +3,32 @@ test1Scores = [0 for i in range(10)]
 test2Scores = [0 for i in range(10)]
 testsAvg = [0.0 for i in range(10)]
 
+highest = -1
+highestName = ""
+lowest = 101
+lowestName = ""
+passed = 0
+failed = 0
+
 for i in range(10):
     testsAvg[i] = (test1Scores[i] + test2Scores[i]) / 2
+
+    if testsAvg[i] >= 50:
+        passed = passed + 1
+    else:
+        failed = failed + 1
+    
+    if testsAvg[i] > highest:
+        highest = testsAvg[i]
+        highestName = studentNames[i]
+    
+    if testsAvg[i] < lowest:
+        lowest = testsAvg[i]
+        lowestName = studentNames[i]
+
+print(f"the number of students who passed is {passed} and failed is {failed}")
+print(f"the highest scoring student is {highestName} at {highest}")
+print(f"the lowest scoring student is {lowestName} at {lowest}")
 
 # find a student and output the average
 # find the highest average and output the name
